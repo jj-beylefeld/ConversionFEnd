67bedd90aefd086edd6c9da7ca12109e2b0d8adf
Jaco Beylefeld <jj.beylefeld@gmail.com>
2021-03-28 21:03:48 +0200

    comment for unit test

--- ConversionToolTests/APIEdgeTests.cs
+++ ConversionToolTests/APIEdgeTests.cs
@@ -6,9 +6,12 @@ namespace ConversionToolTests
     public class APIEdgeTests
     {
         [Fact]
-        public void Test1()
+        public void PreIntegrationTest1()
         {
-
+            /* Test API results with Mock objects - can be performed before actual API is completed. 
+             * This is performed by wrapping the api logic and abstracting it => ConverterAPI 
+             * But I did not write these tests.
+             */
         }
     }
 }
